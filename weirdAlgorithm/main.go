3bd1de5aca48ac23ddf31a66c4a9c304a0941390
wathika-eng <wathika02@gmail.com>
2025-02-02 18:36:00 +0300

    Refactor input handling in main and improve error reporting in askUser function

--- weirdAlgorithm/main.go
+++ weirdAlgorithm/main.go
@@ -10,39 +10,55 @@ import (
 )
 
 func main() {
-	inputOne := askUser()
-	if inputOne <= 0 {
-		log.Fatal("err")
+	var inputOne int
+	var err error
+	for {
+		inputOne, err = askUser()
+		if err == nil && inputOne > 0 {
+			break
+		}
+		log.Println(err)
 	}
 	results := divide(inputOne)
 	fmt.Println(results)
 }
 
-func askUser() int {
+// ask user for a positive number
+//
+// returns -1 if error is present else nil
+func askUser() (int, error) {
 	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Input a positive number: ")
 	input, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatal(err)
+		return -1, fmt.Errorf("error reading input: %v", err)
 	}
 	cleanedInput, err := strconv.Atoi(strings.TrimSpace(input))
 	if err != nil {
-		log.Fatal(err)
+		return -1, fmt.Errorf("error while converting integer: %v", err)
 	}
-	return cleanedInput
+	if cleanedInput <= 0 {
+		return -1, fmt.Errorf("negative input: %d is not a positive number", cleanedInput)
+	}
+	return cleanedInput, nil
 }
 
+// a recursive function
 func divide(n int) []int {
+	// make a slice allocated space of 10^6
 	nums := make([]int, 0, 1000000)
+	// append the first number (userinput)
 	nums = append(nums, n)
+	// run recursivey till n is less than 1
 	for n > 1 {
-
 		if n%2 == 0 {
 			n /= 2
 		} else {
 			n = (n * 3) + 1
 		}
+		// append the results of each function call
 		nums = append(nums, n)
 	}
-
+	// return final results as a slice
 	return nums
 }
